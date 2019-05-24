c7ab8152f90487a2c6facb91317c414b6b1a5cd8
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2019-05-23 22:48:43 -0500

    Skipped ahead a bit and got everything except for the game end logic working

--- src/index.js
+++ src/index.js
@@ -2,38 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return <button className="square">{/* TODO */}</button>;
-  }
-}
+const Square = props => (
+  <button className="square" onClick={props.handleSquareClick}>
+    {props.value}
+  </button>
+);
 
 class Board extends React.Component {
+  state = {
+    player: "X",
+    board: Array(9).fill(null)
+  };
+
+  changePlayer = currentPlayer =>
+    this.setState({ player: currentPlayer === "X" ? "O" : "X" });
+
+  handleSquareClick = index => {
+    if (this.state.board[index] === null) {
+      this.setState({
+        board: (() => {
+          const newBoard = [...this.state.board];
+          newBoard[index] = this.state.player;
+          return newBoard;
+        })()
+      });
+      this.changePlayer(this.state.player);
+    }
+  };
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.board[i]}
+        handleSquareClick={() => this.handleSquareClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.player === "X" ? "O" : "X"}`;
 
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[0, 1, 2].map((i, _, arr) => (
+          <div className="board-row">
+            {arr.map(j => this.renderSquare(j + 3 * i))}
+          </div>
+        ))}
       </div>
     );
   }
